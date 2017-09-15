ddaad8375b583d9f16198564f43bd34b1bbe7fb0
bakea2351 <bakea2351@HRH-IC0027335.wrdsb.ca>
2017-09-15 14:23:49 -0400

    fixed6

---
+++ src/Assignment1/Assignment1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+/**
+ *
+ * @author bakea2351
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
