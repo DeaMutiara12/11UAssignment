95012aeca880b6dad9f8a3ed5d4d8fd4513e1cb0
Donald <Donald@192.168.2.26>
2017-09-19 17:58:42 -0400

    11UAssignment

--- src/Assignment1/A1Q3.java
+++ src/Assignment1/A1Q3.java
@@ -4,6 +4,13 @@
  */
 package Assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
 /**
  *
  * @author bakea2351
@@ -14,6 +21,68 @@ public class A1Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create new city called kitchener
+        City kitchener= new City ();
+        
+        //create new walls
+        new Wall (kitchener, 3,2,Direction.WEST);
+        new Wall (kitchener, 3,2,Direction.NORTH);
+        new Wall (kitchener,2,3,Direction.WEST);
+        new Wall (kitchener,1,3,Direction.WEST);
+        new Wall (kitchener,1,3,Direction.NORTH);
+        new Wall (kitchener,1,3,Direction.EAST);
+        new Wall (kitchener,2,4,Direction.NORTH);
+        new Wall (kitchener,2,4,Direction.EAST);
+        new Wall (kitchener,3,4,Direction.EAST);
+        
+        //create a thing
+        new Thing (kitchener,3,1);
+        //create a robot
+        Robot karel=new Robot (kitchener,3,0,Direction.EAST);
+      
+        
+        //move to the thing
+        karel.move();
+        
+        //change karel's color
+        karel.setColor(Color.BLACK);
+        
+        //have karel pick up the thing
+        karel.pickThing();
+        //have karel turn
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        
+        //have karel drop the thing
+        karel.putThing();
+        
+       //have karel move
+       karel.move();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.turnLeft();
+       karel.move();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.move();
+       karel.move();
+  
+        
+       
     }
 }
